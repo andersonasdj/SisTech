1e6538dd134e6334d52b4dc24373cf66cee47389
Anderson Araujo <andersonasdj@gmail.com>
2024-07-04 20:26:02 -0300

    Ajuste de precissão dos calculos de timesheet

--- src/main/java/br/com/techgol/app/services/TimeSheetService.java
+++ src/main/java/br/com/techgol/app/services/TimeSheetService.java
@@ -39,14 +39,10 @@ public class TimeSheetService {
 		LocalDateTime inicio, fim;
 		
 		if(ini != null  && termino != null ) {
-			System.out.println("SERVICE NAO NULL");
 			inicio = ini.atTime(00, 00, 00);
 			fim = termino.atTime(23, 59, 59);
-			System.out.println("INICIO: " + inicio);
-			System.out.println("FIM: " + fim);
 			return repository.buscarMinutosPorFuncionarioPeriodo(id, inicio, fim);
 		}else {
-			System.out.println("SERVICE NULL");
 			inicio = LocalDateTime.now().withNano(0);
 			fim = LocalDateTime.now().withNano(0);
 			return repository.buscarMinutosPorFuncionarioPeriodo(id, inicio, fim);
